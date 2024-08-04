8196df7d9a5525eaca4d1162dc536f040a400d2e
Kauã Ortiz <kauaortizz@gmail.com>
2024-08-04 02:12:25 -0300

    refactor: correcting rotatex method

--- my_functions.h
+++ my_functions.h
@@ -18,10 +18,11 @@ void rotatex(float theta){
     // Apply matrix for rotation around the x-axis
     // theta: angle (in degrees) to rotate
     theta *= M_PI/180.0;
-    GLfloat matrix[16] = {1, 0, 0, 0,
-                          0, 1, 0, 0,
-                          0, 0, cosf(theta), sinf(theta),
-                          0, 0, -sinf(theta), cosf(theta)};
+    GLfloat matrix[16] = {1, 0,          0,           0,
+                      0, cosf(theta), -sinf(theta), 0,
+                      0, sinf(theta), cosf(theta),  0,
+                      0, 0,          0,           1};
+
     glMultMatrixf(matrix);
 }
 
@@ -56,7 +57,7 @@ void scale(float x, float y, float z){
     glMultMatrixf(matrix);  
 }
 
-void orthogonal(float left, float right, float bottom, float top, float near, float far){
+void orthogonal(float left, float right, float bottom, float top, float znear, float zfar){
     /* 
     Apply matrix for orthogonal projection
 
@@ -66,10 +67,10 @@ void orthogonal(float left, float right, float bottom, float top, float near, fl
     */ 
     float scale_x  =  2.0f/(right - left);
     float scale_y  =  2.0f/(top - bottom);
-    float scale_z  = -2.0f/(far - near);
+    float scale_z  = -2.0f/(zfar - znear);
     float offset_x = -(right+left)/(right-left);
     float offset_y = -(top+bottom)/(top-bottom);
-    float offset_z = -(far+near)  /(far-near);
+    float offset_z = -(zfar+znear)  /(zfar-znear);
 
     float matrix[16] = {scale_x,  0.0f,     0.0f,     0.0f,
                         0.0f,     scale_y,  0.0f,     0.0f,
@@ -79,7 +80,7 @@ void orthogonal(float left, float right, float bottom, float top, float near, fl
     glMultMatrixf(matrix);
 }
 
-void perspective(float left, float right, float bottom, float top, float near, float far){
+void perspective(float left, float right, float bottom, float top, float znear, float zfar){
     /* 
     Apply matrix for perspective projection
 
@@ -88,12 +89,12 @@ void perspective(float left, float right, float bottom, float top, float near, f
     - The camera is at the origin, facing the negative z-axis.
     - Matrix exaplanation: https://www.scratchapixel.com/lessons/3d-basic-rendering/perspective-and-orthographic-projection-matrix/building-basic-perspective-projection-matrix.html
     */ 
-    float scale_x  =  2.0f*near/(right - left);
-    float scale_y  =  2.0f*near/(top - bottom);
-    float scale_z  = -2.0f*far*near/(far - near);
+    float scale_x  =  2.0f*znear/(right - left);
+    float scale_y  =  2.0f*znear/(top - bottom);
+    float scale_z  = -2.0f*zfar*znear/(zfar - znear);
     float offset_x = (right+left)/(right-left);
     float offset_y = (top+bottom)/(top-bottom);
-    float offset_z = -(far+near)  /(far-near);
+    float offset_z = -(zfar+znear)  /(zfar-znear);
 
     float matrix[16] = {scale_x,  0.0f,     0.0f,      0.0f,
                         0.0f,     scale_y,  0.0f,      0.0f,
